4a61838b3c2b0ef124cc6465a0ef53caa16e5f17
GonzaloAlarconRoldan <gonzaloalarconsis@outlook.com>
2022-11-10 00:27:28 -0400

    class 3

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -7,11 +7,12 @@ class Obstacle(Sprite):
 
     def __init__(self, image, type):
         self.image = image
+        self.type = type
         self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
 
-    def update(self, obstacles):
-        self.rect.x -= 5
+    def update(self, game_speed, obstacles):
+        self.rect.x -= game_speed
         if self.rect.x < -self.rect.width:
             obstacles.pop()
 
